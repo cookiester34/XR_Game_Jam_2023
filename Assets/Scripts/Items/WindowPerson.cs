ffcd6f3f40de51916fdf88a14461af5ba4808e75
Joseph Cook <joseph.cook@xrgames.io>
2023-04-28 14:00:00 +0100

    Added Window People to throw items

---
+++ Assets/Scripts/Items/WindowPerson.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Mathematics;
+using CookieUtils;
+using UnityEngine;
+
+public class WindowPerson : MonoBehaviour
+{
+	[SerializeField]
+	private ItemCollectionPoint itemCollectionPoint;
+
+	public bool IsThrowing { get; private set; }
+
+	private Throwable throwable;
+
+	public void SpawnItem(Throwable item)
+	{
+		IsThrowing = true;
+		throwable = Instantiate(item, transform.position, quaternion.identity);
+	}
+
+	private void Update()
+	{
+		if (throwable != null)
+		{
+			throwable.transform.position = Utils.Smoothstep(throwable.transform.position, itemCollectionPoint.transform.position, 0.1f);
+			var distance = Vector3.Distance(throwable.transform.position, itemCollectionPoint.transform.position);
+			if (distance <= 0.1f)
+			{
+				IsThrowing = false;
+				throwable = null;
+			}
+		}
+	}
+}
